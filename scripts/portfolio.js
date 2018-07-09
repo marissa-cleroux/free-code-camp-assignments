5ab9df2f9d9ed6b253abb024a61489b52f23d562
Marissa Cleroux <Marissa.Cleroux@gmail.com>
2018-07-09 18:24:04 -0400

    Completed landing of mobile

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,32 +1,32 @@
-function formatCanvas(){
-    let canvas = document.getElementById('welcome-canvas');
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-    canvas.style.position = 'absolute';
-    canvas.style.zIndex = '-10';
-    let w = canvas.width;
-    let h = canvas.height;
+(function onLoad() {
+    function formatCanvas(){
+        let canvas = document.getElementById('welcome-canvas');
+        canvas.width = innerWidth;
+        canvas.height = innerHeight;
+        let w = canvas.width;
+        let h = canvas.height;
 
-    let ctx = canvas.getContext('2d', "welcome");
-    drawDecal(ctx, w, h);
-}
+        let ctx = canvas.getContext('2d', "welcome");
+        drawDecal(ctx, w, h);
+    }
 
 
-function drawDecal(ctx, w, h){
+    function drawDecal(ctx, w, h){
+        ctx.beginPath();
+        ctx.moveTo(0, h);
+        ctx.lineTo(w, 50);
+        ctx.lineTo(w, h);
+        ctx.closePath();
+        ctx.strokeStyle = '#f0f8ff';
+        ctx.stroke();
+        ctx.fillStyle = '#f0f8ff';
+        ctx.fill();
+    }
 
-    ctx.beginPath();
-    ctx.moveTo(0, h);
-    ctx.lineTo(w, 50);
-    ctx.lineTo(w, h);
-    ctx.closePath();
-    ctx.strokeStyle = '#f0f8ff';
-    ctx.stroke();
-    ctx.fillStyle = '#f0f8ff';
-    ctx.fill();
-}
-
-formatCanvas();
-
-addEventListener('resize', () => {
     formatCanvas();
-});
+
+    addEventListener('resize', () => {
+        formatCanvas();
+    });
+
+})();
